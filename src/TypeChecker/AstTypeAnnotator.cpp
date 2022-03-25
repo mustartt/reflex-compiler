89502b3a4648c738319ed3db16295b8d7fcb3bac
Henry Jiang <h243jian@uwaterloo.ca>
2022-03-25 01:45:21 -0400

    annotate interface members

---
+++ src/TypeChecker/AstTypeAnnotator.cpp
@@ -0,0 +1,156 @@
+//
+// Created by henry on 2022-03-24.
+//
+
+#include <iostream>
+#include <unordered_map>
+#include <AstContextManager.h>
+#include <TypeParser.h>
+
+#include "AstTypeAnnotator.h"
+#include "AstNodes.h"
+#include "TopologicalSort/TopSort.h"
+
+namespace reflex {
+
+TypeParseError::TypeParseError(const std::string &arg)
+    : runtime_error("TypeParserError: " + arg) {}
+
+ClassTypeError::ClassTypeError(const std::string &arg)
+    : runtime_error("ClassTypeError: " + arg) {}
+
+InterfaceTypeError::InterfaceTypeError(const std::string &arg)
+    : runtime_error("InterfaceTypeError: " + arg) {}
+
+AstTypeAnnotator::AstTypeAnnotator(reflex::AstContextManager *astContext,
+                                   TypeContextManager *typeContext)
+    : astContext(astContext), typeContext(typeContext), parser(typeContext) {}
+
+std::vector<ClassDecl *> AstTypeAnnotator::sortClassInheritance() {
+    std::vector<ClassDecl *> classDeclNodes;
+    std::unordered_map<std::string, size_t> classLst;
+    for (auto &classDecl: astContext->getAstNodesByType<ClassDecl>()) {
+        auto decl = dynamic_cast<ClassDecl *>(classDecl.get());
+        auto classname = decl->getClassname();
+        if (classLst.count(classname))
+            throw ClassTypeError{"Duplicate classname " + classname};
+        classLst[classname] = classDeclNodes.size();
+        classDeclNodes.push_back(decl);
+    }
+    TopologicalSort<ClassDecl *> sorter(classDeclNodes);
+    for (size_t i = 0; i < classDeclNodes.size(); ++i) {
+        auto basename = classDeclNodes[i]->getBaseClassname();
+        if (basename) {
+            if (!classLst.count(basename.value()))
+                throw ClassTypeError{"Unknown base class " + basename.value()};
+            sorter.addDirectedEdge(classLst[basename.value()], i);
+        }
+    }
+    return sorter.sort();
+}
+
+std::vector<InterfaceDecl *> AstTypeAnnotator::sortInterfaceInheritance() {
+    std::vector<InterfaceDecl *> interfaceDeclNodes;
+    std::unordered_map<std::string, size_t> interfaceLst;
+    for (auto &interfaceDecl: astContext->getAstNodesByType<InterfaceDecl>()) {
+        auto decl = dynamic_cast<InterfaceDecl *>(interfaceDecl.get());
+        auto interfaceName = decl->getInterfaceName();
+        if (interfaceLst.count(interfaceName))
+            throw InterfaceTypeError{"Duplicate interface " + interfaceName};
+        interfaceLst[interfaceName] = interfaceDeclNodes.size();
+        interfaceDeclNodes.push_back(decl);
+    }
+    TopologicalSort<InterfaceDecl *> sorter(interfaceDeclNodes);
+    for (size_t i = 0; i < interfaceDeclNodes.size(); ++i) {
+        for (auto interface: interfaceDeclNodes[i]->getInterfaces()) {
+            auto basename = interface->getIdent();
+            if (!interfaceLst.count(basename))
+                throw InterfaceTypeError{"Unknown interface " + basename};
+            sorter.addDirectedEdge(interfaceLst[basename], i);
+        }
+    }
+    return sorter.sort();
+}
+
+void AstTypeAnnotator::annotate() {
+    annotateInterfaceDecls();
+}
+
+void AstTypeAnnotator::annotateInterfaceDecls() {
+    auto ordering = sortInterfaceInheritance();
+    for (auto decl: ordering) {
+        decl->accept(this);
+    }
+}
+
+void AstTypeAnnotator::visit(InterfaceDecl *visitable) {
+    // find inherited interface types
+    std::vector<InterfaceType *> inherited;
+    for (auto ident: visitable->getInterfaces()) {
+        auto inheritedInterfaceName = ident->getIdent();
+        auto interfaceTyp = typeContext->getInterfaceTyp(inheritedInterfaceName);
+        if (!interfaceTyp)
+            throw InterfaceTypeError{"Unknown interface " + inheritedInterfaceName};
+        inherited.push_back(interfaceTyp.value());
+    }
+    auto interfaceName = visitable->getName()->getIdent();
+    auto theInterface = typeContext->createOrGetInterfaceTy(interfaceName, inherited);
+    // add interface methods
+    for (auto member: visitable->getSignatures()) {
+        member->accept(this);
+        auto funcDecl = dynamic_cast<FunctionDecl *>(member->getDeclaration());
+        theInterface->addInterfaceMethod(funcDecl->getName()->getIdent(),
+                                         dynamic_cast<MemberType *>(member->getTyp()));
+    }
+    visitable->setTyp(theInterface);
+}
+
+void AstTypeAnnotator::visit(MemberDecl *visitable) {
+    auto modifier = getVisibilityFromString(visitable->getModifier()->getIdent());
+    auto declaration = visitable->getDeclaration();
+    if (auto ptr = dynamic_cast<VariableDecl *>(declaration)) {
+        ptr->accept(this);
+        auto type = typeContext->createMemberTy(nullptr, modifier, ptr->getTyp());
+        if (!type) throw TypeParseError{"Unable to parse MemberDecl VariableType"};
+        visitable->setTyp(type);
+    }
+    if (auto ptr = dynamic_cast<FunctionDecl *>(declaration)) {
+        ptr->accept(this);
+        auto type = typeContext->createMemberTy(nullptr, modifier, ptr->getTyp());
+        if (!type) throw TypeParseError{"Unable to parse MemberDecl FunctionType"};
+        visitable->setTyp(type);
+    }
+    // falls through for class and interface decl
+}
+
+void AstTypeAnnotator::visit(FunctionDecl *visitable) {
+    std::vector<Type *> paramTypes;
+    for (auto p: visitable->getParams()) {
+        p->accept(this);
+        paramTypes.push_back(p->getTyp());
+    }
+    TypeParser typeParser(typeContext);
+    auto retType = typeParser.parseTypeExpr(visitable->getRetTyp());
+    if (!retType)
+        throw TypeParseError{"Unable to parse FunctionDecl type "
+                                 + visitable->getName()->getIdent()};
+    visitable->setTyp(typeContext->getFunctionTy(retType, paramTypes));
+}
+
+void AstTypeAnnotator::visit(VariableDecl *visitable) {
+    auto type = parser.parseTypeExpr(visitable->getVariableType());
+    if (!type)
+        throw TypeParseError{"Unable to parse VariableDecl type "
+                                 + visitable->getName()->getIdent()};
+    visitable->setTyp(type);
+}
+
+void AstTypeAnnotator::visit(ParamDecl *visitable) {
+    auto type = parser.parseTypeExpr(visitable->getParamType());
+    if (!type)
+        throw TypeParseError{"Unable to parse ParamDecl type "
+                                 + visitable->getName()->getIdent()};
+    visitable->setTyp(type);
+}
+
+}
---
+++ src/TypeChecker/AstTypeAnnotator.h
@@ -0,0 +1,85 @@
+//
+// Created by henry on 2022-03-24.
+//
+
+#ifndef REFLEX_SRC_TYPECHECKER_ASTTYPEANNOTATOR_H_
+#define REFLEX_SRC_TYPECHECKER_ASTTYPEANNOTATOR_H_
+
+#include <AstVisitor.h>
+#include <TypeContextManager.h>
+#include <TypeParser.h>
+
+namespace reflex {
+
+class TypeParseError : public std::runtime_error {
+  public:
+    explicit TypeParseError(const std::string &arg);
+};
+
+class ClassTypeError : public std::runtime_error {
+  public:
+    explicit ClassTypeError(const std::string &arg);
+};
+
+class InterfaceTypeError : public std::runtime_error {
+  public:
+    explicit InterfaceTypeError(const std::string &arg);
+};
+
+class AstTypeAnnotator : public AstVisitor {
+    AstContextManager *astContext;
+    TypeContextManager *typeContext;
+    TypeParser parser;
+  public:
+    AstTypeAnnotator(AstContextManager *astContext, TypeContextManager *typeContext);
+    void annotate();
+  public:
+    void visit(Identifier *visitable) override {}
+    void visit(ModuleSelector *visitable) override {}
+    void visit(NumberLit *visitable) override {}
+    void visit(StringLit *visitable) override {}
+    void visit(BoolLit *visitable) override {}
+    void visit(NullLit *visitable) override {}
+    void visit(IdentifierType *visitable) override {}
+    void visit(ArrayType *visitable) override {}
+    void visit(FunctionType *visitable) override {}
+    void visit(ArrayLit *visitable) override {}
+    void visit(ParamDecl *visitable) override;
+    void visit(FunctionLit *visitable) override {}
+    void visit(UnaryExpr *visitable) override {}
+    void visit(BinaryExpr *visitable) override {}
+    void visit(NewExpr *visitable) override {}
+    void visit(CastExpr *visitable) override {}
+    void visit(SelectorExpr *visitable) override {}
+    void visit(IndexExpr *visitable) override {}
+    void visit(ArgumentExpr *visitable) override {}
+    void visit(VariableDecl *visitable) override;
+    void visit(ReturnStmt *visitable) override {}
+    void visit(BreakStmt *visitable) override {}
+    void visit(ContinueStmt *visitable) override {}
+    void visit(IfStmt *visitable) override {}
+    void visit(ForRangeClause *visitable) override {}
+    void visit(ForNormalClause *visitable) override {}
+    void visit(ForStmt *visitable) override {}
+    void visit(WhileStmt *visitable) override {}
+    void visit(EmptyStmt *visitable) override {}
+    void visit(AssignmentStmt *visitable) override {}
+    void visit(IncDecStmt *visitable) override {}
+    void visit(ExpressionStmt *visitable) override {}
+    void visit(Block *visitable) override {}
+    void visit(FunctionDecl *visitable) override;
+    void visit(ClassDecl *visitable) override {}
+    void visit(MemberDecl *visitable) override;
+    void visit(InterfaceDecl *visitable) override;
+    void visit(CompilationUnit *visitable) override {}
+  private:
+    void annotateInterfaceDecls();
+    void annotateClassDecls();
+
+    std::vector<ClassDecl *> sortClassInheritance();
+    std::vector<InterfaceDecl *> sortInterfaceInheritance();
+};
+
+}
+
+#endif //REFLEX_SRC_TYPECHECKER_ASTTYPEANNOTATOR_H_
