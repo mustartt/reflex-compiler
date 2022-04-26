51c1330139417e44c36462c7f091f94dfc565851
Henry Jiang <h243jian@uwaterloo.ca>
2022-04-26 00:41:48 -0700

    refactor ast parser

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <SourceManager.h>
 #include <Lexer.h>
+#include <Parser.h>
+#include <ASTContext.h>
 
 int main(int argc, char *argv[]) {
     using namespace reflex;
@@ -8,39 +10,9 @@ int main(int argc, char *argv[]) {
     SourceManager srcManager;
     auto &file = srcManager.open("language_test.reflex");
 
+    ASTContext astContext;
     Lexer lexer(file, file.content(), getTokenDescription(), getKeywordDescription());
-    while (lexer.hasNext()) {
-        auto tok = lexer.nextToken();
-
-        if (!tok.isTrivial())
-            tok.getLocInfo()->printSourceRegion(std::cout, true);
-    }
-
-    //    AstContextManager ctx;
-//    Parser parser(&lexer, &ctx);
-//
-//    auto root = parser.parseCompilationUnit();
-//    AstPrinter printer(std::cout);
-//    TypeContextManager manager;
-//
-//    AstRecordTypeAnnotator recordTypeAnnotator{manager};
-//    auto table = recordTypeAnnotator.annotate(root);
-//    table->resetPrefixCounter();
-//
-//    AstDeclTypeAnnotator declTypeAnnotator{manager, table};
-//    declTypeAnnotator.annotate(root);
-//    table->resetPrefixCounter();
-//
-//    AstExpressionAnnotator expressionAnnotator{manager, table};
-//    expressionAnnotator.annotate(root);
-//    table->resetPrefixCounter();
-//
-//    manager.dump(std::cout);
-//    std::cout << std::string(75, '=') << std::endl;
-//    table->printScope(std::cout, 0);
-//    std::cout << std::string(75, '=') << std::endl;
-//    printer.visit(root);
-//    std::cout << std::string(75, '=') << std::endl;
+    Parser parser(astContext, lexer);
 
     return 0;
 }
